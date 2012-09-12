4543e4c6ec66ab6045d34b06aa19ab981ae7ac92
R van der Heijden <rik@c-ict.com>
2012-09-12 20:26:42 +0200

    forgot to use mobile_detect before template include

--- src/index.php
+++ src/index.php
@@ -12,7 +12,7 @@ $lang = Lang::instance();
 if($lang->rtl()) Config::set('rtl', 1);
 
 if(!is_int(Config::get('firstdayofweek')) || Config::get('firstdayofweek')<0 || Config::get('firstdayofweek')>6) Config::set('firstdayofweek', 1);
-
+use_mobile();
 define('TEMPLATEPATH', MTTPATH. 'themes/'.Config::get(MTT_TEMPLATE_VAR).'/');
 
 require(TEMPLATEPATH. 'index.php');
